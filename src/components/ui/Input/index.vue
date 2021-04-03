58545c41cfe7f0661830d80ca561389030004f31
Matt Rose <mattroseinbox@gmail.com>
2021-04-04 00:46:41 +0430

    feat: add input ui component

---
+++ src/components/ui/Input/index.vue
@@ -0,0 +1,20 @@
+<style src="./style.css" scoped></style>
+<template>
+  <div class="input-container" :class="className">
+    <input :type="type" :placeholder="placeholder" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import type { InputType } from './@types/index'
+
+export default defineComponent({
+  name: "Input",
+  props: {
+    type: String as () => InputType,
+    placeholder: String,
+    className: String
+  },
+});
+</script>
