b780db56e2b7b492a28ab15e26c8acd5db7bfaf7
Matt Rose <mattroseinbox@gmail.com>
2021-04-04 15:37:33 +0430

    feat: add Logo ui component

---
+++ src/components/ui/Logo/index.vue
@@ -0,0 +1,17 @@
+<style src="./style.css" scoped></style>
+<template>
+  <router-link to="/" :class="className">
+    Reflect
+  </router-link>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Logo",
+  props: {
+    className: String,
+  },
+});
+</script>
