ed51e35cf6d9a85963fb22b3a97e44c478e14c0a
Matt Rose <mattroseinbox@gmail.com>
2021-04-20 15:32:15 +0430

    perf: improve apollo config

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,7 +1,33 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client/core";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloLink,
+  split,
+} from "@apollo/client/core";
+import { setContext } from "@apollo/client/link/context";
 import { API_URL } from "@/common/config";
+import { createUploadLink } from "apollo-upload-client";
+
+const cache = new InMemoryCache();
+
+const httpLink = createUploadLink({
+  uri: API_URL,
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = JSON.parse(localStorage.getItem("token") || "");
+  console.log(headers);
+  return {
+    headers: {
+      ...headers,
+      token: token || "",
+    },
+  };
+});
+
+const link = ApolloLink.from([authLink, httpLink]);
 
 export const defaultClient = new ApolloClient({
-  uri: API_URL,
-  cache: new InMemoryCache(),
+  link,
+  cache,
 });
